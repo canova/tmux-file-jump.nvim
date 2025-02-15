092fc42d4f6276ec7c56de99964857c6874e7251
Nazım Can Altınova <canaltinova@gmail.com>
2025-02-15 15:39:20 +0100

    refactor: Improve the capture script

--- scripts/capture.sh
+++ scripts/capture.sh
@@ -3,13 +3,13 @@
 pattern=$1
 
 function capture_panes() {
-    local pane captured current_pane captured_line
+    local pane captured current_pane
     captured=""
     current_pane=$(tmux display -pt "${TMUX_PANE:?}" '#{pane_index}')
 
     for pane in $(tmux list-panes -F "#{pane_index}"); do
-      if [[ $pane != $current_pane ]]; then
-            captured+="$(tmux capture-pane -pJS - -t $pane)"
+      if [[ $pane != "$current_pane" ]]; then
+            captured+="$(tmux capture-pane -pJS - -t "$pane")"
             captured+=$'\n'
       fi
     done
